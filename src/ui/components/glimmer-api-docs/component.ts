dfbcdde277818f6519516f9276529061de050dca
Ricardo Mendes <rokusu@gmail.com>
2017-03-23 17:58:55 +0000

    all the single templates

--- src/ui/components/glimmer-api-docs/component.ts
+++ src/ui/components/glimmer-api-docs/component.ts
@@ -1,5 +1,17 @@
 import Component from "@glimmer/component";
 
 export default class MyComponent extends Component {
-
+    get model() {
+        return {
+            main: { title: "Hello Glimmer" },
+            menu: [
+                {
+                    name: "Ember",
+                    children: [
+                        { name: "Component" }
+                    ]
+                }
+            ]
+        }
+    }
 }
