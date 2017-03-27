a8f7c691e86b50e7d528a38ff6f76fe228074a19
Tom Dale <tom@tomdale.net>
2017-03-27 11:49:23 -0700

    Script for generating API doc JSON

---
+++ bin/build-api-json.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+const path = require('path');
+
+const GIT_REPOS = [
+  ['glimmer-component', 'https://github.com/glimmerjs/glimmer-component'],
+  ['glimmer-application', 'https://github.com/glimmerjs/glimmer-application']
+];
+
+try {
+  fs.mkdirSync('repos');
+} catch (e) { }
+
+GIT_REPOS.forEach(([repoPath, url]) => {
+  let stat;
+  try {
+    stat = fs.statSync(path.join('repos', repoPath));
+  } catch (e) {
+    execSync(`git clone "${url}"`, {
+      cwd: 'repos'
+    });
+  }
+  execSync('git pull --rebase', {
+    cwd: path.join('repos', repoPath)
+  });
+});
+
+execSync('jtd docs-source/docs-config.json');
