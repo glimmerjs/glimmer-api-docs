a5ecd5f59306e8a78593b4562be4dc28ca783c7c
asakusuma <asa.kusuma@gmail.com>
2017-03-27 21:32:42 -0700

    Fix rootUrl in dev

--- config/environment.js
+++ config/environment.js
@@ -52,6 +52,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.rootURL = '/api/';
   }
 
   if (environment === 'test') {
