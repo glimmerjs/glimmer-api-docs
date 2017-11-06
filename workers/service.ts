c5fe2f99e994e7d6576fc484ba6b1afe4a72f636
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2017-11-06 16:50:42 +0100

    remove debug output

--- workers/service.ts
+++ workers/service.ts
@@ -16,7 +16,6 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(URLS_TO_CACHE);
       })
   );
